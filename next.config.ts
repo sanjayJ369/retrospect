791959e325288c0a35ad263b45362ca48ae490b9
sanjayj369 <j.sanjay336699@gmail.com>
2025-09-17 16:57:41 +0530

    Added deploy script to package.json for deploying with Wrangler

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
